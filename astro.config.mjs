c9c909ee624da83b3d83d96ac736814ed7687029
mzyxsl <3305114625@qq.com>
2026-04-19 10:47:43 +0800

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-    server: {
-        fs: {
-            allow: ['../..']
-        }
-    }
+    plugins: [tailwindcss()]
   }
 });
