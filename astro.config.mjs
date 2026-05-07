476813c92a8fe81acac92a73caa7f18621722e70
mzyxsl <3305114625@qq.com>
2026-05-07 21:31:20 +0800

    update: 添加Open Graph

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://about.mzyxsl.com',
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
